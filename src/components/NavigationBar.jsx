b47a0c1fa3f0ccfbca28e8e86bd0a71a9cc04ebf
정은우 <wjddmsdn_8976@naver.com>
2023-11-25 18:15:48 +0900

    design App.css 폰트 및 body 기본 설정

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,30 +1,51 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavigationBar = () => {
   return (
-    <nav id="navbar">
-    <div className="navLeft">
-      <div className="hamburger">
-        <button>바</button>
-      </div>
-      <div className="nav_logo">
-        <a href="#">NDB</a>
-      </div>
-    </div>
-    <div className="navRight">
-      
-      <div>
-        <form action="/login">
-          <button type="submit" className="nav_login_button">Login</button>
-        </form>
-      </div>
-      <div>
-        <form action="#">
-          <button type="button" className="nav_join_button">Join</button>
-        </form>
-      </div>
-    </div>
-  </nav>
+        <nav id="main_navbar">
+          <div className="main_navLeft">
+            <div id="sidebar_toggle">
+              <input type="checkbox" />
+              <span></span>
+              <span></span>
+              <span></span>
+              <ul id="leftside">
+                <a><li>닉네임</li></a>
+                <a><li>회원정보</li></a>
+                <a><li>정보</li></a>
+                <a href="#" className="subs_btn_open"><li>구독</li></a>
+              </ul>
+            </div>
+            <div className="main_nav_logo">
+              <a href="http://localhost:3003/index">NDB</a>
+            </div>
+            <div className="main_navMiddle">
+              <div className="main_Nav_search">
+                <select name="main_language" id="main_navSearchbar" className="main_searchLanguage" style={{width: "50px", height: "30px"}}>
+                  <option value=" ">언어</option>
+                  {/* ...기타 옵션... */}
+                </select>
+                <input type="text" className="main_searchBar" name="main_searchBar_Name"/>
+                <div className="main_autocomplete"></div>
+                <button type="submit" className="main_search_button" onClick={() => fetchData()}>
+                  {/* 아이콘은 필요에 따라 추가하세요. */}
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="main_navRight">
+            <ul className="popularSerch">
+              {/* ...기타 리스트 아이템... */}
+            </ul>
+            <div className="checkbox-wrapper-2">
+              <input type="checkbox" id="main_Nav_themecheck" className="sc-gJwTLC ikxBAC" onClick={() => is_checked()}/>
+            </div>
+            <div id="main_btnWrap">
+              <button type="button" className="main_signupBtn main_modalBtn">log out</button>
+            </div>
+          </div>
+        </nav>   
   )
 }
 
