da87526509d26b10d7edced5002b4d06f13a6f62
Hyun jin <blackduvet52@gmail.com>
2023-11-25 19:20:56 +0900

    #7 로그인 페이지 css적용 및 레이아웃 구성
    - 네비게이션바 삭제
    - src/assets/login폴더 생성 및 소셜 로그인 파일 추가
    - Index.css 파일 내용 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,19 @@ import Join from './pages/Join'
 import Subs from './pages/Subs'
 import MyPage from './pages/MyPage'
 import './App.css'
-import NavigationBar from './components/NavigationBar'
+
 import Login from './pages/Login'
 
 function App() {
   return (
     <Router>
-      <div>
-        <NavigationBar />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/join" element={<Join />} />
-          <Route path="/subs" element={<Subs />} />
-          <Route path="/mypage" element={<MyPage />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/subs" element={<Subs />} />
+        <Route path="/mypage" element={<MyPage />} />
+      </Routes>
     </Router>
   )
 }
