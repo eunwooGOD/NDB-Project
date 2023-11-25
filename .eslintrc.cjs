e44ecd7292fccfe963c2e1eaebe8ff8162f8153b
Hyun jin <blackduvet52@gmail.com>
2023-11-25 16:14:29 +0900

    REACT+ VITE 환경셋팅

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+// .eslintrc.cjs
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    // Add your custom rules here
+  },
+};
