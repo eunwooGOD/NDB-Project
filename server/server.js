7a15cc11b6bff3028f8320861520e8f134d6fdba
KimHyeoungGyun <138184795+KimHyeoungGyun@users.noreply.github.com>
2023-11-30 20:54:51 +0900

    서버 구성  -김형균-

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require('express'); 
 const app = express();
 
 
 const indexRouter = require('./routes') //index는 default값 이기 때문에 생략이 가능
 const userRouter = require('./routes/user')
-
+const codeRouter = require('./service/gptres')
 const path = require("path");
 const cors = require('cors')
 
 // post 방식으로 데이터 주고 받기 위한 body-parser
 const bodyParser = require('body-parser');
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 // cors 오류 해결을 위한 미들웨어
 app.use(cors());
@@ -21,15 +21,17 @@ app.use(express.urlencoded({extended : true}))
 // router 
 app.use('/user', userRouter);
 app.use('/', indexRouter);
+app.use('/gptres', codeRouter)
 
 
-
-
-
-
+app.get('*', (req, res) => {
+    console.log('main router');
+    // __dirname은 현재 스크립트가 위치한 디렉토리를 나타냅니다.
+    res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+});
 // 서버 포트번호
 app.set('port', process.env.PORT|| 3002);
 
 app.listen(app.get('port'),()=>{
-    console.log('port waiting ...')
+    console.log('3002 port waiting ...')
 });
--- server/service/gptres.js
+++ server/service/gptres.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const path = require("path");
 
-
+router.get('/codeExam', (req, res)=>{
+    console.log('code router')
+})
 
 module.exports = router;
