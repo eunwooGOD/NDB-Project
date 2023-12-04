082da7dfa55c0b782ffd994cba34ae2712e18e2a
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2023-12-04 21:13:47 +0900

    feat: mainpage search function

---
+++ src/components/main/MainSearch.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const MainSearch = () => {
+  return <div>this is main search</div>
+  // return(
+  //   <div class="main_wrap main_wrap_backImg_night">
+  //               <!----------------------  메인창 뒤 글자흐름  ------------------------>
+
+  //               <div class="main_searchWrap">
+  //                   
+  //                   <!-- [230802_예정]flex basis로 최적화 예정(현재는 width, px로 강제 지정) -->
+
+  //                   <div class="main_search main_search_night">
+  //                       <select name="main_language" id="" class="main_searchLanguage2" style="width:50px;height:30px;">
+  //                           <option value=" ">언어</option>
+  //                           <option value="html">html</option>
+  //                           <option value="css">css</option>
+  //                           <option value="javascript">javascript</option>
+  //                           <option value="java">java</option>
+  //                           <option value="python">python</option>
+  //                           <option value="c_language">C언어</option>
+  //                       </select>
+  //                       <!-- <i class="fa-solid fa-chevron-down" style="color: #d1d1d1;"></i> -->
+  //                       <input type="text" class="main_searchBar2" name="main_searchBar_Name">
+  //                       <!-- 자동완성 단어 리스트 -->
+  //                       <div class="main_autocomplete"></div>
+  //                       <button type="submit" class="main_search_button" onclick="user_Btn()">
+  //                           <i class="fa-solid fa-magnifying-glass" style="color: #d1d1d1;"></i>
+  //                       </button>
+  //                   </div>
+
+  //                   <!-- 검색창 아래 태그 -->
+  //                   <!-- <div class="main_TagWrap">
+  //                   사용자에게 추천하는 검색어
+  //                   <ul>
+  //                       <li>반복문</li>
+  //                       <li>if문</li>
+  //                       <li>grid</li>
+  //                   </ul>
+  //               </div> -->
+  //               </div>
+  //           </div>
+  // )
+}
+
+export default MainSearch
